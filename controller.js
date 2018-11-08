f2416eb3eb29631a08d94ecf12908122acaed468
jonas <jonas-gruempel@gmx.de>
2018-11-08 23:09:19 +0100

    v. 0.3 added draggable cards

--- controller.js
+++ controller.js
@@ -1,47 +1,53 @@
 window.onload = function onload () {
 
-  $('#add_popup').animate({
-    'opacity': '0',
-    'margin-left': '-=5000px',
-    'margin-top': '-=5000px'
-  }, 'fast');
-
   $('#add_kanban').click(function(){
-    $('#add_popup').css({
-      'height': '350px',
-      'width': '375px'
-    });
-    $('#add_text').css({'opacity': '1'});
-    $('#button_cancel').css({'opacity': '1'});
-    $('#button_submit').css({'opacity': '1'});
-    $('#add_popup').animate({
-      'opacity': '1',
-      'margin-left': '+=5000px',
-      'margin-top': '+=5000px'
-    }, 'fast');
+    var text = prompt('Card text:');
+    if(text !== null) {
+      if(text !== '') {
+        generateKanbanBoard(text);
+      }else{
+        alert('You have to enter a card text!');
+      }
+    }
   });
 
-  $('#button_submit').click(function(){
-    $('#add_popup').animate({
-      'opacity': '0',
-      'margin-left': '-=5000px',
-      'margin-top': '-=5000px'
-    }, 'fast');
+  $('#load_kanban').click(function(){
+    var path = prompt('Enter the path to JSON-file:');
+    if(path !== null) {
 
-    $('#add_text').animate({'opacity': '0'}, 'fast');
-    $('#button_cancel').animate({'opacity': '0'}, 'fast');
-    $('#button_submit').animate({'opacity': '0'}, 'fast');
+    }
   });
 
-  $('#button_cancel').click(function(){
-      $('#add_popup').animate({
-        'opacity': '0',
-        'margin-left': '-=5000px',
-        'margin-top': '-=5000px'
-      }, 'fast');
-      $('#add_text').animate({'opacity': '0'}, 'fast');
-      $('#button_cancel').animate({'opacity': '0'}, 'fast');
-      $('#button_submit').animate({'opacity': '0'}, 'fast');
+  $('#save_kanban').click(function(){
+    var path = prompt('Enter save path:');
+    if(path !== null) {
+
+    }
   });
+
+  $('#refresh_kanban').click(function(){
+    location.reload();
+  });
+
+  $('#clear_kanban').click(function(){
+
+  });
+
+  function generateKanbanBoard (text) {
+    var card = document.createElement('div');
+    card.setAttribute('id', generateId());
+    card.setAttribute('class', 'card');
+    card.setAttribute('draggable', 'true');
+    card.setAttribute('ondragstart', 'drag(event)');
+    card.setAttribute('title', 'press left for moving');
+    card.innerHTML = text;
+    var backlog = document.getElementById('backlog');
+    backlog.appendChild(card);
+    document.getElementById('add_text').value = "";
+  }
+
+  function generateId () {
+     return '_' + Math.random().toString(36).substr(2, 9);
+  }
 
 }
